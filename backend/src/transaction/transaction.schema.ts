13e1c0f9d8141d238023e59db8b405ea390b3ba2
joao43 <joao@stolzlabs.com>
2025-01-06 10:24:30 -0300

    refactor: small change

--- backend/src/transaction/transaction.schema.ts
+++ backend/src/transaction/transaction.schema.ts
@@ -3,6 +3,7 @@ import * as mongoose from 'mongoose';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 
 import { HydratedDocument } from 'mongoose';
+import { v4 as uuidv4 } from 'uuid';
 
 export type TransactionDocument = HydratedDocument<Transaction>;
 
@@ -11,7 +12,10 @@ export class Transaction {
   @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Users', required: true })
   userId: mongoose.Types.ObjectId;
 
-  @Prop({ required: true })
+  @Prop({
+    required: true,
+    default: uuidv4,
+  })
   transactionId: string;
 
   @Prop({ required: true })
