047dcbcab4dbea865e6b00a7dec35678b8d3f78b
joao43 <joao@stolzlabs.com>
2025-01-07 18:56:16 -0300

    refactor: change fastify for express

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,16 @@
+import * as express from 'express';
+
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
 import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(express()),
+  );
 
+  // Configuração de CORS
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
@@ -12,4 +19,5 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
